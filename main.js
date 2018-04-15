1f0d45772c12c258e7a670c148eb028cf7bcba65
onlymx13 <onlymx13@gmail.com>
2018-04-15 07:39:18 -0500

    May be better now

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var textArray;
 var outputText;
 var allIndeces;
+var oldTextArray;
 
 function sentenceCase(str) {
     var str = str.toLowerCase().replace(/\si\s/g, ' I ');
@@ -28,37 +29,72 @@ String.prototype.addSpace = function() {
     return this + " ";
 }
 
+function speak(text, callback) {
+    var u = new SpeechSynthesisUtterance();
+    u.text = text;
+    u.lang = 'en-US';
+
+    u.onend = function() {
+        if (callback) {
+            callback();
+        }
+    };
+
+    u.onerror = function(e) {
+        if (callback) {
+            callback(e);
+        }
+    };
+
+    speechSynthesis.speak(u);
+}
+
 function extrapolateOne(text) {
     textArray = text.toUpperCase().split(" ");
     outputText = textArray[Math.floor(textArray.length * Math.random())];
-    textArray.push("*");
+    textArray.push("\n");
     i = 0;
-    while (outputText.charAt(outputText.length - 1) != "*" && i < 3 * textArray.length) {
+    while (outputText.charAt(outputText.length - 1) != "\n" && i < 3 * textArray.length) {
         i++;
         outputText = outputText.addSpace();
-        allIndeces = getAllIndeces(textArray, outputText.split(" ")[outputText.split(" ").length - 2]);
-        outputText = outputText + textArray[1 + allIndeces[Math.floor((allIndeces.length) * Math.random())]];
+        loopOne();
     }
-    speak(sentenceCase(outputText));
-
-    function speak(text, callback) {
-        var u = new SpeechSynthesisUtterance();
-        u.text = text;
-        u.lang = 'en-US';
-
-        u.onend = function() {
-            if (callback) {
-                callback();
-            }
-        };
-
-        u.onerror = function(e) {
-            if (callback) {
-                callback(e);
-            }
-        };
-
-        speechSynthesis.speak(u);
-    }
-    document.getElementById('output').innerHTML = sentenceCase(outputText);
+    finish();
 }
+
+function loopOne() {
+    allIndeces = getAllIndeces(textArray, outputText.split(" ")[outputText.split(" ").length - 2]);
+    outputText = outputText + textArray[1 + allIndeces[Math.floor((allIndeces.length) * Math.random())]];
+}
+
+function extrapolateTwo(text) {
+    oldTextArray = text.toUpperCase().split(" ");
+    textArray.length = oldTextArray.length - 1;
+    for (var i = 0; i < oldTextArray.length - 1; i++) {
+        textArray[i] = [oldTextArray[i], oldTextArray[i + 1]];
+    }
+    outputText = textArray[Math.floor(textArray.length * Math.random())];
+    outputText = outputText.addSpace();
+    allIndeces = getAllIndeces(textArray, outputText.split(" ")[outputText.split(" ").length - 2]);
+    outputText = outputText + textArray[1 + allIndeces[Math.floor((allIndeces.length) * Math.random())]];
+    i = 0;
+    while (i < 3 * textArray.length) {
+        i++;
+        outputText = outputText.addSpace();
+        loopTwo();
+    }
+    finish();
+}
+
+function loopTwo() {
+    allIndeces = getAllIndeces(textArray, [outputText.split(" ")[outputText.split(" ").length - 3], outputText.split(" ")[outputText.split(" ").length - 2]); outputText = outputText + (textArray[1 + allIndeces[Math.floor((allIndeces.length) * Math.random())]])[1]; outputText = outputText + (textArray[1 + allIndeces[Math.floor((allIndeces.length) * Math.random())]])[2];
+    if(allIndeces===[]){
+        loopOne();
+    } else {
+    outputText = outputText + (textArray[1 + allIndeces[Math.floor((allIndeces.length) * Math.random())]])[2];
+    }
+}
+function finish() {
+        speak(sentenceCase(outputText));
+        document.getElementById('output').innerHTML = sentenceCase(outputText);
+    }
