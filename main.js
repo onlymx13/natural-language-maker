4b85f9ce5db3a300bfbe35e0b018b45d38c2f9cd
onlymx13 <onlymx13@gmail.com>
2018-04-15 06:46:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,57 +1,64 @@
 var textArray;
 var outputText;
 var allIndeces;
-function sentenceCase(str){
-	var str = str.toLowerCase().replace(/\si\s/g, ' I ');
-	str = str.replace(/ ti/g, ' TI');
-	str = str.replace(/ ram/g, ' RAM');
-	str = str.replace(/ plus silver edition/g, ' Plus Silver Edition');
-	str=str.charAt(0).toUpperCase()+str.slice(1);
-	for(i=0;i<str.length;i++){
-	if(str.charAt(i)=="."){
-	str=str.slice(0,i+2)+str.charAt(i+2).toUpperCase()+str.slice(i+3);
-	}
-	}
-	return str;
+
+function sentenceCase(str) {
+    var str = str.toLowerCase().replace(/\si\s/g, ' I ');
+    str = str.replace(/ ti/g, ' TI');
+    str = str.replace(/ ram/g, ' RAM');
+    str = str.replace(/ plus silver edition/g, ' Plus Silver Edition');
+    str = str.charAt(0).toUpperCase() + str.slice(1);
+    for (i = 0; i < str.length; i++) {
+        if (str.charAt(i) == ".") {
+            str = str.slice(0, i + 2) + str.charAt(i + 2).toUpperCase() + str.slice(i + 3);
+        }
+    }
+    return str;
 }
+
 function getAllIndeces(arr, val) {
-    var indeces = [], i;
-    for(i = 0; i < arr.length; i++)
+    var indeces = [],
+        i;
+    for (i = 0; i < arr.length; i++)
         if (arr[i] === val)
             indeces.push(i);
     return indeces;
 }
-String.prototype.addSpace=function(){return this+" ";}
-function makeText(text){
-textArray=text.toUpperCase().split(" ");
-outputText=textArray[Math.floor(textArray.length*Math.random())];
-textArray.push("*");
-i=0;
-while(outputText.charAt(outputText.length-1)!="*" && i<3*textArray.length) {
-i++;
-outputText=outputText.addSpace();
-allIndeces = getAllIndeces(textArray,outputText.split(" ")[outputText.split(" ").length-2]);
-outputText=outputText+textArray[1+allIndeces[Math.floor((allIndeces.length)*Math.random())]];
+String.prototype.addSpace = function() {
+    return this + " ";
 }
-speak(sentenceCase(outputText));
-function speak(text, callback) {
-    var u = new SpeechSynthesisUtterance();
-    u.text = text;
-    u.lang = 'en-US';
- 
-    u.onend = function () {
-        if (callback) {
-            callback();
-        }
-    };
- 
-    u.onerror = function (e) {
-        if (callback) {
-            callback(e);
-        }
-    };
- 
-    speechSynthesis.speak(u);
-}
-document.getElementById('output').innerHTML=sentenceCase(outputText);
+
+function extrapolateOne(text) {
+    textArray = text.toUpperCase().split(" ");
+    outputText = textArray[Math.floor(textArray.length * Math.random())];
+    textArray.push("*");
+    i = 0;
+    while (outputText.charAt(outputText.length - 1) != "*" && i < 3 * textArray.length) {
+        i++;
+        outputText = outputText.addSpace();
+        allIndeces = getAllIndeces(textArray, outputText.split(" ")[outputText.split(" ").length - 2]);
+        outputText = outputText + textArray[1 + allIndeces[Math.floor((allIndeces.length) * Math.random())]];
+    }
+    speak(sentenceCase(outputText));
+
+    function speak(text, callback) {
+        var u = new SpeechSynthesisUtterance();
+        u.text = text;
+        u.lang = 'en-US';
+
+        u.onend = function() {
+            if (callback) {
+                callback();
+            }
+        };
+
+        u.onerror = function(e) {
+            if (callback) {
+                callback(e);
+            }
+        };
+
+        speechSynthesis.speak(u);
+    }
+    document.getElementById('output').innerHTML = sentenceCase(outputText);
 }
