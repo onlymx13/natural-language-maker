ec776d69be6657204a99ea39362d5f7378a8608e
onlymx13 <onlymx13@gmail.com>
2018-04-16 10:18:09 -0500

    Maybe fix undefined?

--- main.js
+++ main.js
@@ -90,6 +90,7 @@ function extrapolateThree(text) {
     var rand = Math.floor(textArray.length * Math.random());
     outputText = textArray[rand][0] + " " + textArray[rand][1] + " " + textArray[rand][2];
     textArray.push([textArray[textArray.length - 1][1], textArray[textArray.length - 1][2], "*"]);
+    textArray.push([textArray[textArray,length - 2][2], "*", "*"]);
             i = 0;
             while (outputText.charAt(outputText.length - 1) != "*" && i < 3 * textArray.length) {
                 i++;
